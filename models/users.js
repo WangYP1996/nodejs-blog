14531639276b0d46b30eb5f22ee7ef59f861d8ca
cobomb1996 <455797271@qq.com>
2019-07-05 16:08:16 +0800

    Login Function

--- models/users.js
+++ models/users.js
@@ -2,5 +2,12 @@ const User = require('../lib/mongo').User
 module.exports = {
   create: function create (user) {//创建用户
     return User.create(user).exec()
+  },
+  //4.8 登录
+  getUserByName: function getUserByName (name) {
+    return User
+      .findOne({ name: name })
+      .addCreatedAt()
+      .exec()
   }
 }
