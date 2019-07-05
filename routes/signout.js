ac392b03e73ac22ad57918339948395f62c9c041
cobomb1996 <455797271@qq.com>
2019-07-05 12:50:01 +0800

    signout function

--- routes/signout.js
+++ routes/signout.js
@@ -4,7 +4,11 @@ const router = express.Router()
 const checkLogin = require('../middlewares/check').checkLogin
 
 router.get('/', checkLogin, function (req, res, next) {
-  res.send('登出')
+  //res.send('登出')
+  //4.8修改
+  req.session.user = null
+  req.flash('success', '登出成功')
+  res.redirect('/posts')
 })
 
 module.exports = router
